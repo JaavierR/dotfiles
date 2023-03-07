a74c59380a2f12b8b39cc40fb13133523f07d93a
Javier Reyes <javier.reys@gmail.com>
2023-03-07 14:47:25 -0300

    feat: update fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,26 +1,37 @@
+source ~/.config/fish/alias.fish
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+# Configure Jump
+status --is-interactive; 
+
+# Fish syntax highlighting
+set -g fish_color_autosuggestion '555'  'brblack'
+set -g fish_color_cancel -r
+set -g fish_color_command --bold
+set -g fish_color_comment red
+set -g fish_color_cwd green
+set -g fish_color_cwd_root red
+set -g fish_color_end brmagenta
+set -g fish_color_error brred
+set -g fish_color_escape 'bryellow'  '--bold'
+set -g fish_color_history_current --bold
+set -g fish_color_host normal
+set -g fish_color_match --background=brblue
+set -g fish_color_normal normal
+set -g fish_color_operator bryellow
+set -g fish_color_param cyan
+set -g fish_color_quote yellow
+set -g fish_color_redirection brblue
+set -g fish_color_search_match 'bryellow'  '--background=brblack'
+set -g fish_color_selection 'white'  '--bold'  '--background=brblack'
+set -g fish_color_user brgreen
+set -g fish_color_valid_path --underline
+
+# Install Starship
 starship init fish | source
-
-alias vim="nvim"
-alias lla="ll -a"
-alias cat="bat"
-
-# Git
-alias gs="git status -sb"
-alias gaa="git add -A"
-alias gco="git checkout"
-alias gp="git push"
-alias gl="git pull"
-
-# Laravel
-alias sail="[ -f sail ] && sh sail || sh vendor/bin/sail"
-
-# Docker
-alias d="docker"
-alias dc="docker compose"
 
 # pnpm
 set -gx PNPM_HOME "/home/javier/.local/share/pnpm"
