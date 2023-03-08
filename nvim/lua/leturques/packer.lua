008f60edb38aa193c4fb362b8b606e7e1c51061f
Javier Reyes <javier.reys@gmail.com>
2023-03-08 03:09:35 -0300

    feat: add one dark colorscheme

--- nvim/lua/leturques/packer.lua
+++ nvim/lua/leturques/packer.lua
@@ -7,21 +7,40 @@ vim.cmd [[packadd packer.nvim]]
 require('packer').reset()
 require('packer').init({
     compile_path = vim.fn.stdpath('data')..'/site/plugin/packer_compiled.lua',
-    --display = {
-    --    open_fn = function()
-    --        return require('packer.util').float({ border = 'solid' })
-    --    end,
-    --},
+    display = {
+        open_fn = function()
+            return require('packer.util').float({ border = 'solid' })
+        end,
+    },
 })
 
 local use = require('packer').use
 
+
 -- Packer can manage itself
 use 'wbthomason/packer.nvim'
+
+-- One Dark Theme.
+use({
+    'jessarcher/onedark.nvim',
+    config = function()
+        vim.cmd('colorscheme onedark')
+
+        vim.api.nvim_set_hl(0, 'FloatBorder', {
+            fg = vim.api.nvim_get_hl_by_name('NormalFloat', true).background,
+            bg = vim.api.nvim_get_hl_by_name('NormalFloat', true).background,
+        })
+    end,
+})
 
 -- Commenting support.
 use 'tpope/vim-commentary'
 
+-- Add, change, and delete surrounding text.
+use 'tpope/vim-surround'
+
+
+-- End of plugins
 vim.cmd([[
     augroup packer_user_config
         autocmd!
