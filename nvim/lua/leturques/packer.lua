d3b57efb39244b0392ecb909418775cf4406f02a
Javier Reyes <javier.reys@gmail.com>
2023-03-08 17:56:25 -0300

    feat: add keybind and personalization to telescope

--- nvim/lua/leturques/packer.lua
+++ nvim/lua/leturques/packer.lua
@@ -22,16 +22,22 @@ use 'wbthomason/packer.nvim'
 
 -- One Dark Theme.
 use({
-    'jessarcher/onedark.nvim',
-    config = function()
-        vim.cmd('colorscheme onedark')
+        'jessarcher/onedark.nvim',
+        config = function()
+            vim.cmd('colorscheme onedark')
 
-        vim.api.nvim_set_hl(0, 'FloatBorder', {
-            fg = vim.api.nvim_get_hl_by_name('NormalFloat', true).background,
-            bg = vim.api.nvim_get_hl_by_name('NormalFloat', true).background,
-        })
-    end,
-})
+            vim.api.nvim_set_hl(0, 'FloatBorder', {
+                    fg = vim.api.nvim_get_hl_by_name('NormalFloat', true).background,
+                    bg = vim.api.nvim_get_hl_by_name('NormalFloat', true).background,
+                })
+
+            -- Make the cursor line bg invisible
+            vim.api.nvim_set_hl(0, 'CursorLineBg', {
+                    fg= vim.api.nvim_get_hl_by_name('CursorLine', true).background,
+                    bg = vim.api.nvim_get_hl_by_name('CursorLine', true).background,
+                })
+        end,
+    })
 
 -- Commenting support.
 use 'tpope/vim-commentary'
@@ -39,16 +45,20 @@ use 'tpope/vim-commentary'
 -- Add, change, and delete surrounding text.
 use 'tpope/vim-surround'
 
+-- Add more languages.
+use 'sheerun/vim-polyglot'
+
 -- Fuzzy finder
 use({
     'nvim-telescope/telescope.nvim',
     requires = {
         'nvim-lua/plenary.nvim',
         'kyazdani42/nvim-web-devicons',
+        'nvim-telescope/telescope-live-grep-args.nvim',
         { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' },
     },
     config = function()
-        require('telescope').setup()
+        require('leturques/plugins/telescope')
     end,
 })
 
