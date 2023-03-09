b6786379a45e97ffd9395348908533375d6618d6
Javier Reyes <javier.reys@gmail.com>
2023-03-09 17:25:20 -0300

    feat: add new options

--- nvim/lua/leturques/set.lua
+++ nvim/lua/leturques/set.lua
@@ -4,10 +4,9 @@ vim.opt.nu = true
 vim.opt.relativenumber = true
 
 vim.opt.expandtab = true
-vim.opt.shiftwidth = 4
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
 vim.opt.smartindent = true
 
 vim.opt.wrap = false
@@ -26,16 +25,21 @@ vim.opt.smartcase = true
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
+vim.opt.list = true -- enable the below listchars
+vim.opt.listchars = { tab = '▸ ', trail = '·' }
 vim.opt.fillchars:append({ eob = " " }) -- remove the ~ from the end of buffer
 
 vim.opt.splitbelow = true
 vim.opt.splitright = true
 
 vim.opt.scrolloff = 8
+vim.opt.sidescrolloff = 8
 
 -- vim.opt.clipboard = "unnamedplus" -- Use system clipboard
 
 vim.opt.confirm = true -- ask for confirmation instead of erroring
+
+vim.opt.signcolumn = 'yes:2'
 
 vim.opt.undofile = true -- persistent undo
 -- vim.opt.backup = true -- automatically save a backup file
