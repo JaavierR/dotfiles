590d61f2b67adca779b3aedeee18416c86ed1723
Javier Reyes <javier.reys@gmail.com>
2023-03-10 17:18:48 -0300

    fix: add option to tree sitter to enable indentation on php files

--- nvim/lua/leturques/plugins/treesitter.lua
+++ nvim/lua/leturques/plugins/treesitter.lua
@@ -2,6 +2,11 @@ require('nvim-treesitter.configs').setup({
     ensure_installed = 'all',
     highlight = {
       enable = true,
+      disable = { 'NvimTree' },
+      additional_vim_regex_highlighting = false,
+    },
+    indent = {
+      enable = true,
     },
     context_commentstring = {
       enable = true,
