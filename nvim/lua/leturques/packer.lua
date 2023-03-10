5666dc62489137df5a8dfa6fc867be7696e0f2f7
Javier Reyes <javier.reys@gmail.com>
2023-03-10 01:28:06 -0300

    feat: floating terminal

--- nvim/lua/leturques/packer.lua
+++ nvim/lua/leturques/packer.lua
@@ -105,6 +105,21 @@ use({
     requires = 'tpope/vim-rhubarb',
   })
 
+-- Floating terminal.
+use({
+    'voldikss/vim-floaterm',
+    config = function ()
+      vim.g.floaterm_width = 0.8
+      vim.g.floaterm_height = 0.8
+      vim.keymap.set('n', '<F1>', ':FloatermToggle<CR>')
+      vim.keymap.set('t', '<F1>', '<C-\\><C-n>:FloatermToggle<CR>')
+      vim.cmd([[
+        highlight link Floaterm CursorLine
+        highlight link FloatermBorder CursorLineBg
+      ]])
+    end
+  })
+
 -- End of plugins
 vim.cmd([[
     augroup packer_user_config
