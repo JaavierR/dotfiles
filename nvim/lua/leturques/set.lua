58f783c8cbb0c06c5e0ae991712b39f0b683cf06
Javier Reyes <javier.reys@gmail.com>
2023-03-10 17:09:35 -0300

    refactor: fix indentation default options in vim

--- nvim/lua/leturques/set.lua
+++ nvim/lua/leturques/set.lua
@@ -4,9 +4,10 @@ vim.opt.nu = true
 vim.opt.relativenumber = true
 
 vim.opt.expandtab = true
-vim.opt.shiftwidth = 2
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+
 vim.opt.smartindent = true
 
 vim.opt.wrap = false
