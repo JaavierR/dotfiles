4e635f1d502bcc1cc4becbda1a72e94694c9c893
Javier Reyes <javier.reys@gmail.com>
2023-04-01 18:50:30 -0300

    refactor: add floaterm border

--- nvim/lua/leturques/packer.lua
+++ nvim/lua/leturques/packer.lua
@@ -181,7 +181,6 @@ use({
 use({
   'voldikss/vim-floaterm',
   config = function()
-    vim.g.floaterm_borderchars = ''
     vim.g.floaterm_width = 0.8
     vim.g.floaterm_height = 0.8
     vim.keymap.set('n', '<F1>', ':FloatermToggle<CR>')
