064deda23c8e54d4587292a4ee8c62e4184be2c1
Javier Reyes <javier.reys@gmail.com>
2023-05-08 18:51:34 -0400

    feat: add function to check gh pr in fzf

--- fish/config.fish
+++ fish/config.fish
@@ -29,6 +29,10 @@ function cx
   cd $argv && l
 end
 
+function ghpr
+  GH_FORCE_TTY=100% gh pr list | fzf --ansi --preview 'GH_FORCE_TTY=100% gh pr view {1}' --preview-window down --header-lines 3 | awk '{print $1}' | xargs gh pr checkout
+end
+
 # Autostart tmux
 if status is-interactive
 and not set -q TMUX
