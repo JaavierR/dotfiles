3436ec5e5618a04eb09663cf3566334ba86be3c5
Javier Reyes <javier.reys@gmail.com>
2023-06-04 03:25:35 -0400

    refactor: format set.lua

--- nvim/lua/leturques/set.lua
+++ nvim/lua/leturques/set.lua
@@ -12,8 +12,10 @@ vim.opt.smartindent = true
 
 vim.opt.wrap = false
 
-vim.opt.wildmode = "longest:full,full" -- complete the longest common match, and allow tabbing the results to fully complete them
-vim.opt.completeopt = "menuone,longest,preview" -- complete the longest common match, and allow tabbing the results to fully complete them
+vim.opt.wildmode =
+"longest:full,full"                             -- complete the longest common match, and allow tabbing the results to fully complete them
+vim.opt.completeopt =
+"menuone,longest,preview"                       -- complete the longest common match, and allow tabbing the results to fully complete them
 
 vim.opt.title = true
 vim.opt.mouse = 'a' --enable mouse for all modes
@@ -27,7 +29,7 @@ vim.opt.smartcase = true
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
-vim.opt.list = true -- enable the below listchars
+vim.opt.list = true                     -- enable the below listchars
 vim.opt.listchars = { tab = '▸ ', trail = '·' }
 vim.opt.fillchars:append({ eob = " " }) -- remove the ~ from the end of buffer
 
