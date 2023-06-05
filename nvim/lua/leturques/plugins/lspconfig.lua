40397e796407466dedf8994eab50f01220268c18
Javier Reyes <javier.reys@gmail.com>
2023-06-05 00:26:33 -0400

    fix(astro): override on_new_config method in astro lsp This enable the possibility to load the typescript server from the project folder

--- nvim/lua/leturques/plugins/lspconfig.lua
+++ nvim/lua/leturques/plugins/lspconfig.lua
@@ -35,14 +35,23 @@ lspconfig.volar.setup({
 -- Tailwind CSS
 lspconfig.tailwindcss.setup({ capabilities = capabilities })
 
+local util = require('lspconfig.util')
+local function get_typescript_server_path(root_dir)
+  local project_root = util.find_node_modules_ancestor(root_dir)
+  return project_root and (util.path.join(project_root, 'node_modules', 'typescript', 'lib')) or ''
+end
+
 -- Astro
 lspconfig.astro.setup({
   capabilities = capabilities,
-  init_options = {
-    typescript = {
-      tsdk = vim.fs.normalize('~/Library/pnpm/global/5/node_modules/typescript/lib')
-    }
-  },
+  on_new_config = function(new_config, new_root_dir)
+    if
+        vim.tbl_get(new_config.init_options, 'typescript')
+        and not new_config.init_options.typescript.tsdk
+    then
+      new_config.init_options.typescript.tsdk = get_typescript_server_path(new_root_dir)
+    end
+  end
 })
 
 -- JSON
