94d81e0f657dfdf7e409d460441c640818e1bba7
Javier Reyes <javier.reys@gmail.com>
2024-02-15 13:31:45 -0300

    refactor: modify max lines in pyton

--- nvim/lua/leturques/plugins/lspconfig.lua
+++ nvim/lua/leturques/plugins/lspconfig.lua
@@ -80,7 +80,7 @@ null_ls.setup({
 
     -- Python formatting
     null_ls.builtins.formatting.black.with({
-      extra_args = { '--line-length', '120' },
+      extra_args = { '--line-length', '88' },
     }),
 
     null_ls.builtins.formatting.isort,
