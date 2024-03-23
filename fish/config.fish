fe61e33e5e4b8e2f8532430709e0ef52bcd98555
Javier Reyes <javier.reys@gmail.com>
2024-03-23 19:20:27 -0300

    chore: add reference for the solution

--- fish/config.fish
+++ fish/config.fish
@@ -34,6 +34,7 @@ function ghpr
 end
 
 # Remove if the starship new line is set to true
+# https://stackoverflow.com/questions/65722822/fish-shell-add-newline-before-prompt-only-when-previous-output-exists
 function postexec_test --on-event fish_postexec
    echo
 end
